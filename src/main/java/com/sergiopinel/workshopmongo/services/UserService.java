5eb789d87ca424b745579b851a8e539b597c5f7f
Sergio Pinel <pinelbh@gmail.com>
2018-10-11 11:49:12 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/sergiopinel/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.sergiopinel.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sergiopinel.workshopmongo.domain.User;
+import com.sergiopinel.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
